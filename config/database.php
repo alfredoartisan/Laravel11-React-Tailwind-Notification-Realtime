bf273da1150b28ea4b4cb88eb7bdd377bb787a87
Alfredo Pascual Luque <alfredopascualluque7@gmail.com>
2025-01-13 10:16:13 +0100

    Minor change

--- config/database.php
+++ config/database.php
@@ -37,7 +37,7 @@ return [
             'database' => env('DB_DATABASE', database_path('database.sqlite')),
             'prefix' => '',
             'foreign_key_constraints' => env('DB_FOREIGN_KEYS', true),
-
+        ],
         'mysql' => [
             'driver' => 'mysql',
             'url' => env('DB_URL'),
